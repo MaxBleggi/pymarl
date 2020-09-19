1878444bdb97203ab50f6be5741c564f70c6776f
Sina Masoud-Ansari <sina.masoud.ansari@gmail.com>
2020-09-19 22:29:55 +0000

    fixed loop position for batch_size_run

--- src/run.py
+++ src/run.py
@@ -232,20 +232,22 @@ def run_sequential(args, logger):
                     model_batch = model_learner.generate_batch(buffer, rollout_batch_size, rl_iterations)
                     model_buffer.insert_episode_batch(model_batch)
 
+
                 if model_buffer.can_sample(args.batch_size):
-                    episode_sample = model_buffer.sample(args.batch_size)
+                    for _ in range(args.batch_size_run):
+                        episode_sample = model_buffer.sample(args.batch_size)
 
-                    # truncate batch to only filled timesteps
-                    #max_ep_t = episode_sample.max_t_filled()
-                    #episode_sample = episode_sample[:, :max_ep_t]
+                        # truncate batch to only filled timesteps
+                        #max_ep_t = episode_sample.max_t_filled()
+                        #episode_sample = episode_sample[:, :max_ep_t]
 
-                    if episode_sample.device != args.device:
-                        episode_sample.to(args.device)
+                        if episode_sample.device != args.device:
+                            episode_sample.to(args.device)
 
-                    # train RL agent
-                    learner.train(episode_sample, runner.t_env, rl_iterations)
-                    rl_iterations += 1
-                    print(f"Model RL iteration {rl_iterations}, t_env: {runner.t_env}")
+                        # train RL agent
+                        learner.train(episode_sample, runner.t_env, rl_iterations)
+                        rl_iterations += 1
+                        print(f"Model RL iteration {rl_iterations}, t_env: {runner.t_env}")
 
             if not collect_episodes and rl_iterations > 0 and rl_iterations % args.model_update_interval == 0:
                 print(f"Time to update model")
@@ -262,16 +264,16 @@ def run_sequential(args, logger):
             buffer.insert_episode_batch(episode_batch)
 
             if buffer.can_sample(args.batch_size):
-                episode_sample = buffer.sample(args.batch_size)
-
-                # Truncate batch to only filled timesteps
-                max_ep_t = episode_sample.max_t_filled()
-                episode_sample = episode_sample[:, :max_ep_t]
-
-                if episode_sample.device != args.device:
-                    episode_sample.to(args.device)
-
                 for _ in range(args.batch_size_run):
+                    episode_sample = buffer.sample(args.batch_size)
+
+                    # Truncate batch to only filled timesteps
+                    max_ep_t = episode_sample.max_t_filled()
+                    episode_sample = episode_sample[:, :max_ep_t]
+
+                    if episode_sample.device != args.device:
+                        episode_sample.to(args.device)
+
                     learner.train(episode_sample, runner.t_env, episode)
 
         # Execute test runs once in a while
