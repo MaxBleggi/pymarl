43c410e18a2346a3d4b8ef3eade5bdf810ce51a0
Sina Masoud-Ansari <sina.masoud.ansari@gmail.com>
2021-01-24 06:01:42 +0000

    back to rts-k-d-0 config

--- src/modules/models/reward.py
+++ src/modules/models/reward.py
@@ -10,11 +10,9 @@ class RewardModel(nn.Module):
     def __init__(self, input_size, output_size, hidden_size):
         super().__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, output_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, xt):
         xt = F.relu(self.fc1(xt))
-        xt = F.relu(self.fc2(xt))
-        xt = self.fc3(xt)
+        xt = self.fc2(xt)
         return xt
